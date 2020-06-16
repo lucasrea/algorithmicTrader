b8da9c30267327f8564de590c0d2f58e02379807
lucasrea <lucas.rea1998@gmail.com>
2020-06-16 19:59:09 -0400

    Add files via upload
    Added support for train mode vs regular trading mode, as well as added more comments in the code

--- env.py
+++ env.py
@@ -6,56 +6,91 @@ from get_data import GetData
 
 class Environment:
 
-    def __init__(self, WINDOW_LENGTH, EPOCHS, BATCH_SIZE, symbol):
+    def __init__(self, WINDOW_LENGTH, EPOCHS, BATCH_SIZE, symbol, train=True):
+
+        """
+        Constants used for things like
+        - WINDOW_LENGTH : how many elements of data are in a state
+        - EPOCHS : number of episodes the model is trained for
+        - BATCH_SIZE : how many cycles we go until we fit the agent's model
+        """
         self.WINDOW_LENGTH = WINDOW_LENGTH
         self.EPOCHS = EPOCHS
         self.BATCH_SIZE = BATCH_SIZE
 
-        self.data = GetData(symbol).format_data()
+        """
+        Variables for the environment data (prices of the stock)
+        Train variable to determine if were being run in training mode or trading mode
+        """
+        self.stock = GetData(symbol, train)
+        self.data = self.stock.format_data()
+        self.symbol = symbol
         self.data_len = len(self.data)
+        self.train = train
 
+        """
+        Parameters that are subject to reset after every training episode
+        """
         self.total_profit = 0
-        self.max__profit = 0
-        self.already_bought = False
+        self.episode_profit = 0
         self.buy_count = 0
         self.sell_count = 0
         self.history = []
 
 
 
-    def get_state(self, t, n):
+    def get_state(self, t=0):
         """
-            Function to break the data up into window sized chunks
-            :return:
+        Function to break the data up into window sized chunks
+        Returns an n sized array up until t
+
+        If we are in train mode, we already have all of the data, so we use the t iterator to determine where we want the state to end
+        Otherwise, we need to pull the next minute of data, and retrieve the last WINDOW_LENGTH elements
+
+        :return: a numpy array of length WINDOW_SIZE with the sigmoid transformed data
         """
 
         def sigmoid(x):
             return 1 / (1 + math.exp(-x))
 
-        d = t - n + 1
-        block = self.data[d:t + 1] if d >= 0 else np.append(-d * [self.data[0]], self.data[0:t + 1])  # pad with t0
+        n = self.WINDOW_LENGTH + 1
+
+        if self.train: # A length check for when we run the live trader
+            d = t - n
+            block = self.data[d:t + 1] if d >= 0 else np.append(-d * [self.data[0]], self.data[0:t + 1])  # pad with t0
+
+        else: # If we are not training, we just need to grab the last WINDOW_SIZE + 1 # of  elements
+            self.data = self.stock.update_data() # Get the updated minute-by-minute data
+            block = self.data[ self.data_len - n : self.data_len + 1 ]
+
         res = []
         for i in range(n - 1):
             res.append(sigmoid(block[i + 1] - block[i]))
 
         return np.array([res])
 
+
     def determine_reward(self, agent, action, t):
         """
         Function that will determine the reward for the agent depending on the action
-        For a buy, we check how many better options there were (where the price was cheaper)
-        For a sell, we check if there was a better time to sell (where the price was more expensive
+        *** Only used during training ***
+
+        -For a buy, we check how many better options there were (where the price was cheaper)
+        -For a sell, we check if there was a better time to sell (where the price was more expensive
 
         :param agent:
         :param action:
-        :param t:
-        :return:
+        :param t: index of data
+        :return: reward value, the more positive -> better
         """
 
         reward = 0
         profit = 0
 
-        if action == 1 :  # Buy only if we already have less than 6 active buy orders
+        if action == 0:
+            self.history.append('H')
+
+        if action == 1:  # Buy only if we already have less than 6 active buy orders
         # Having a limit on the number of buys is a more realistic way of trading
 
             self.buy_count += 1
@@ -72,26 +107,27 @@ class Environment:
                     break
 
             # For every better option, subtract 0.1 from a total of 1 possible point
-            reward = 0.5 - (0.1 * min_count)
+            #reward = 0.2 - (0.02 * min_count)
 
             #print('Buy at ' + str(self.data[t]))
             # print(str(min_count) + ' possible buys that were better, got a reward of ' + str(reward) + ' out of 1 \n')
 
         elif action == 2 and len(agent.inventory) > 0:  # sell
-            self.history.append('S')
-            self.sell_count += 1
+
             bought_price = min(agent.inventory)
             agent.inventory.remove(bought_price)
             reward = max(self.data[t] - bought_price, 0)
             profit = self.data[t] - bought_price
 
             self.total_profit += profit
+            self.episode_profit += profit
+            self.history.append('S')
+            self.sell_count += 1
 
-            self.already_bought = False
 
-            if profit > 0: # We made profit, it's at least a decent trade
+            #if profit > 0: # We made profit, it's at least a decent trade
                 # If we were profitable, we add an immediate 0.7 / 1.0
-                reward = 0.8 * profit
+            reward = 0.8 * profit
 
             # Check if we could have bought at a higher price
             max_count = 0
@@ -105,10 +141,21 @@ class Environment:
             # For every price that was potentially higher, we subtract 0.03 from a possible 0.3 for this component
             reward += 0.2 - (0.02 * max_count)
 
-
             #print('Sold at ' + str(self.data[t]))
             # print(str(max_count) + ' possible buys that were better, got a reward of ' + str(reward) + ' out of 1 \n')
             #print('Profit of : ' + str(profit))
         return reward, profit
 
 
+
+
+    def reset_params(self):
+        """
+        Function to reset some parameters at the begining of every episode
+        :return:
+        """
+
+        self.buy_count = 0
+        self.sell_count = 0
+        self.history = []
+        self.episode_profit = 0
--- get_data.py
+++ get_data.py
@@ -1,36 +1,69 @@
 import datetime
 import pandas as pd
 import yfinance as yf
+import math
+import numpy as np
 from finta import TA
+from scipy.special import expit
+from sklearn.preprocessing import StandardScaler
 
 
 class GetData:
     """
     Class to retrieve the training data for a given stock
     """
-    NUM_DAYS = 5_000
-    INDICATORS = ['EMA', 'RSI', 'BBANDS',  'MACD']
-    def __init__(self, stock):
-        """
-        Function to get the past 730 days of data for a stock, hourly
-        :param stock:
-        """
+    NUM_DAYS = 5
+    INDICATORS = ['EMA', 'RSI', 'MACD']
+    NOTUSED_STATE = ['high', 'low', 'open', 'Adj Close', 'volume']
 
-
-        start =  (datetime.date.today() - datetime.timedelta( self.NUM_DAYS ) )
-        end = datetime.datetime.today()
-        self.data = yf.download(stock, start=start, end=end, interval='1d')
-        self.data.rename(columns={"Close": 'close', "High": 'high', "Low": 'low', 'Volume': 'volume', 'Open': 'open'}, inplace=True)
-        self.data['label'] = 0
+    def __init__(self, stock, train):
+        """
+        Function to get the past 5 days of data for a stock, minute by minute for training
+        For live data, just need todays 1m interval data
+        :param stock: symbol of the stock
+        """
+        if train:
+            start =  (datetime.date.today() - datetime.timedelta( self.NUM_DAYS ) )
+            end = datetime.datetime.today()
+            self.data = yf.download(stock, start=start, end=end, interval='1m')
+            self.data.rename(columns={"Close": 'close', "High": 'high', "Low": 'low', 'Volume': 'volume', 'Open': 'open'}, inplace=True)
+        else:
+            start = datetime.date.today()
+            end = datetime.datetime.today()  + datetime.timedelta( 1 )
+            self.data = yf.download(stock, start=start, end=end, interval='1m')
+            self.data.rename(columns={"Close": 'close', "High": 'high', "Low": 'low', 'Volume': 'volume', 'Open': 'open'}, inplace=True)
 
 
     def get_indicator_data(self):
+        """
+        Function that adds the indicators to the data table used in analysis
+        Can add whichever indicators you would need
+        :return:
+        """
 
         for indicator in self.INDICATORS:
             ind_data = eval('TA.' + indicator + '(self.data)')
             if not isinstance(ind_data, pd.DataFrame):
                 ind_data = ind_data.to_frame()
-
             self.data = self.data.merge(ind_data, left_index=True, right_index=True)
+
+    def update_data(self, symbol):
+        start = datetime.date.today()
+        end = datetime.datetime.today() + datetime.timedelta(1)
+        self.data = yf.download(symbol, start=start, end=end, interval='1m')
+        self.data.rename(columns={"Close": 'close', "High": 'high', "Low": 'low', 'Volume': 'volume', 'Open': 'open'},inplace=True)
+
+        return self.format_data()
+
+    def format_data(self):
+        """
+        Return the data in a form that can be passed into the neural net (numpy array)
+        :return:
+        """
+
+        # Filter out the other columns and transform into a np array
+        state = self.data.drop( self.NOTUSED_STATE, axis=1 )
+        self.vec = state.values.flatten()
+        return self.vec
 
 
